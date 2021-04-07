dd7062c63e8779f7cd1248963948ad8c10e30424
shuchitasingh <singhshuchita5@gmail.com>
2021-04-07 16:18:59 +0530

    Added:single playerat position 0

--- SinglePlayerUC1.java
+++ SinglePlayerUC1.java
@@ -2,7 +2,7 @@ public class SinglePlayerUC1 {
 
 	public static void main(String[] args) {
 
-		System.out.println("Wellcome to snake ladder game: ");
+		System.out.println("Welcome to snake ladder game: ");
 
 		int startPosition = 0;
 
