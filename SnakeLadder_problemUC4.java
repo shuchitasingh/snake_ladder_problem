b979b5e987b6f3e9f10377e08613f6bd5c07140e
shuchitasingh <singhshuchita5@gmail.com>
2021-04-07 16:33:05 +0530

    Added:winning position 100

---
+++ SnakeLadder_problemUC4.java
@@ -0,0 +1,35 @@
+public class SnakeLadder_problemUC4 {
+
+	public static void main(String[] args) {
+
+		//System.out.println("Welcome to snake ladder game: ");
+
+		int position = 0;
+		int TempPosition;
+		int minimum = 1;
+		int maximum = 6;
+
+
+		while(position < 100)
+		{
+			int randomValue = (int) Math.floor(Math.random() * (maximum - minimum + 1) + minimum);  //Math.random()*(max-min+1)+min
+//			System.out.println("randomvalue" + randomValue);
+
+			int randomOption = (int) (Math.random() * 10) % 3;
+//			System.out.println("randomoption" + randomOption);
+
+			if(randomOption == 0 && position == 0)
+				position = 0;
+
+			else if(randomOption == 1 && position < 100)
+				position += randomValue;
+
+			else if(randomOption ==2)
+			{
+				position -= randomValue;
+				if(position < 0 )
+					position = 0;
+			}
+		System.out.println("position" + position);
+		}
+	}
